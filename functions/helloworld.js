d28e370157edb07d12f7b72f4bbe313ce36e3262
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-09-22 05:45:30 +0900

    feat: add CORS to functions/helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -3,5 +3,13 @@ export function onRequest(context) {
     data: "Hello, world!"
   };
 
-  return new Response(JSON.stringify(json));
+  return new Response(JSON.stringify(json), {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
 }
