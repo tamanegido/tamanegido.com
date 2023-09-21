e828a7194b198514414d89032b45fcdb8717be42
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-09-22 05:54:19 +0900

    feat: revert functions/helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -3,13 +3,5 @@ export function onRequest(context) {
     data: "Hello, world!"
   };
 
-  return new Response(JSON.stringify(json), {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Max-Age': '86400',
-    },
-  });
+  return new Response(JSON.stringify(json));
 }
